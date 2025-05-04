787838d0cf1bef988183c382f2ef8b142b040df8
Bruno Salles <bsalles428@gmail.com>
2025-05-04 17:32:43 -0300

    feat: add initial frontend structure.

---
+++ app/frontend/app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1>Olá Mundo!</h1>
+      <button className=" bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+    </div>
+  );
+}
+
+export default App;
